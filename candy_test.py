9330c93d8911e1f9ac9915e25d456efefa39c7de
Jim Barry <jbarry1506@gmail.com>
2020-10-11 18:51:26 -0400

    fix file loc

--- candy_test.py
+++ candy_test.py
@@ -2,4 +2,4 @@
 
 import shutil
 
-shutil.move("/home/Pictures/PiCam/teststill_gomiami.jpg", "/home/Code/jcbhub/latest.jpg")
+shutil.move("/home/pi/Pictures/PiCam/teststill_gomiami.jpg", "/home/pi/Code/jcbhub/latest.jpg")
