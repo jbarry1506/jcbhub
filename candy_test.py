76241e758e6a75e2cea822854a6d7ecce7eb27d9
Jim Barry <jbarry1506@gmail.com>
2020-10-11 23:21:33 -0400

    first successful jenkins call

--- candy_test.py
+++ candy_test.py
@@ -1,5 +1,24 @@
 #!/usr/bin/python3
 
 import shutil
+import jenkins
+import pout
+import pprint
+import vars
 
-shutil.move("/home/pi/Pictures/PiCam/teststill_gomiami.jpg", "/home/pi/Code/jcbhub/latest.jpg")
+# detect button push
+# trigger logic power
+    # 10 seconds
+    # strobe light
+# trigger camera
+# trigger pennywise servo
+# trigger sound effect
+# trigger candy drop
+# move file
+# start jenkins build
+# reset Pennywise
+
+# shutil.move("/home/pi/Pictures/PiCam/teststill_gomiami.jpg", "/home/pi/Code/jcbhub/latest.jpg")
+
+server = jenkins.Jenkins(vars.jenkins_server, username=vars.jenkins_user, password=vars.jenkins_password)
+pprint.pprint(server.get_all_jobs())
