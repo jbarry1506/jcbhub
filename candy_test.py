63d759e432727c96005e05cbae78725ee3234c55
Jim Barry <jbarry1506@gmail.com>
2020-10-12 21:37:56 -0400

    snap_pic()

--- candy_test.py
+++ candy_test.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 
+from time import sleep
+from picamera import PiCamera
 import shutil
 import jenkins
 import pprint
 import vars
 
 # detect button push
+# display countdown on screen
 # trigger logic power
     # 10 seconds
     # strobe light
@@ -16,8 +19,13 @@ import vars
 # move file
 # start jenkins build
 # reset Pennywise
+# display website
 
 # shutil.move("/home/pi/Pictures/PiCam/teststill_gomiami.jpg", "/home/pi/Code/jcbhub/latest.jpg")
+
+
+def display_webpage():
+    pass
 
 
 def button_detect():
@@ -29,6 +37,15 @@ def logic_switch():
 
 
 def snap_pic():
+    camera = PiCamera()
+    camera.resolution = (1024, 768)
+    camera.start_preview()
+    # Camera warm-up time
+    sleep(2)
+    camera.capture('latest.jpg')
+
+
+def countdown():
     pass
 
 
@@ -56,5 +73,6 @@ def reset_pennywise():
     pass
 
 
+snap_pic()
 server = jenkins.Jenkins(vars.jenkins_server, username=vars.jenkins_user, password=vars.jenkins_password)
 pprint.pprint(server.get_all_jobs())
