23b3892eef2a1a0a0a72fd6d939c8e8c977b604c
Jim Barry <jbarry1506@gmail.com>
2020-10-19 20:37:15 -0400

    email_pic

--- candy_test.py
+++ candy_test.py
@@ -2,11 +2,12 @@
 
 from time import sleep
 from picamera import PiCamera
-import datetime
-import shutil
-import subprocess
+import datetime, smtplib, shutil, subprocess, pprint
+from email.mime.multipart import MIMEMultipart 
+from email.mime.text import MIMEText 
+from email.mime.base import MIMEBase 
+from email import encoders 
 # import jenkins
-import pprint
 import vars
 
 # detect button push
@@ -29,10 +30,14 @@ def display_webpage():
 
 
 def button_detect():
+    # detect input from button
+    # signal to execute the rest of the program
     pass
 
 
 def logic_switch():
+    # turn off 'normally on' 
+    # turn on 'normally off'
     pass
 
 
@@ -45,15 +50,116 @@ def snap_pic():
     camera.capture('/home/pi/Pictures/PiCam/latest.jpg')
 
 
+def email_picture(fl):
+    # fl = file location
+    em = vars.em
+    pw = vars.xv
+
+    # try:
+    #     server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    #     server.ehlo()
+    #     server.login(em, pw)
+    # except Exception as e:
+    #     print('Something went wrong with Login.')
+    #     print(e)
+
+    # try:
+    #     server_ssl = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    #     server_ssl.ehlo()   # optional
+    #     # ...send emails
+    # except Exception as e:
+    #     print('Something went wrong in SSL')
+    #     print(e)
+
+# https://www.geeksforgeeks.org/send-mail-attachment-gmail-account-using-python/
+#-------------------------------------------------------------------------------
+
+    fromaddr = vars.em
+    toaddr = vars.em
+    
+    # instance of MIMEMultipart 
+    msg = MIMEMultipart() 
+    
+    # storing the senders email address   
+    msg['From'] = fromaddr 
+    
+    # storing the receivers email address  
+    msg['To'] = toaddr 
+
+    # storing the subject  
+    msg['Subject'] = "Scaredy-Pi"
+    
+    # string to store the body of the mail 
+    body = "WE GOT ONE!!!"
+    
+    # attach the body with the msg instance 
+    msg.attach(MIMEText(body, 'plain')) 
+    
+    # open the file to be sent  
+    filename = fl
+    attachment = open(fl, "rb") 
+    
+    # instance of MIMEBase and named as p 
+    p = MIMEBase('application', 'octet-stream') 
+    
+    # To change the payload into encoded form 
+    p.set_payload((attachment).read()) 
+    
+    # encode into base64 
+    encoders.encode_base64(p) 
+    
+    p.add_header('Content-Disposition', "attachment; filename= %s" % filename) 
+    
+    # attach the instance 'p' to instance 'msg' 
+    msg.attach(p) 
+  
+    # creates SMTP session 
+    s = smtplib.SMTP('smtp.gmail.com', 587) 
+    
+    # start TLS for security 
+    s.starttls() 
+    
+    # Authentication 
+    s.login(fromaddr, vars.xv) 
+    
+    # Converts the Multipart msg into a string 
+    text = msg.as_string() 
+    
+    # sending the mail 
+    s.sendmail(fromaddr, toaddr, text) 
+    
+    # terminating the session 
+    s.quit() 
+#-------------------------------------------------------------------------------
+    # email_text = ("""\
+    # From: {}
+    # To: {}
+    # Subject: {}
+
+    # {}
+    # """).format(sent_from, ", ".join(to), subject, body)
+
+    # try:
+    #     server.sendmail(sent_from, to, email_text)
+    #     server.close()
+
+    #     print('Email sent!')
+    # except Exception as e:
+    #     print("The email wasn't sent.")
+    #     print(e)
+
+
 def countdown():
-    pass
+    # triggered from button detect
 
 
 def candy_drop():
+    # triggered from button_detect
     pass
 
 
 def pennywise():
+    # triggered from button_detect
     pass
 
 
@@ -83,10 +189,15 @@ def push_pic():
     subprocess.call(["git", "push"])
 
 
+def reset():
+    pass
+
+
+# initialize pyglet needs to happen before the rest of the program
 snap_pic()
 move_file()
 # sleep(2)
 push_pic()
 display_webpage()
-# server = jenkins.Jenkins(vars.jenkins_server, username=vars.jenkins_user, password=vars.jenkins_password)
-# pprint.pprint(server.get_all_jobs())
+server = jenkins.Jenkins(vars.jenkins_server, username=vars.jenkins_user, password=vars.jenkins_password)
+pprint.pprint(server.get_all_jobs())
