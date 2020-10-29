fab9c2d1b1510aeba67a55337e770ae34f18d3db
pi3 <jbarry1506@gmail.com>
2020-10-29 17:16:14 -0400

    merge conflict

--- sp_tst_button.py
+++ sp_tst_button.py
@@ -7,5 +7,5 @@ GPIO.setup(16, GPIO.OUT)
 # Input from pushbutton, using internal pulldown resistor. Other button wire will connect to 5V pin. 
 GPIO.setup(12, GPIO.IN, pull_up_down = GPIO.PUD_UP) 
 
-if GPIO.Input(12) == 0:
+if GPIO.input(12) == 0:
     print("Button Pusher!  Button Pusher!")
