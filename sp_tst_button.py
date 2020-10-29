6d0214e096169e43f38b4aa58d9a6daa3bbf4813
Jim Barry <jbarry1506@gmail.com>
2020-10-29 17:14:57 -0400

    add while to test button

--- sp_tst_button.py
+++ sp_tst_button.py
@@ -7,5 +7,6 @@ GPIO.setup(16, GPIO.OUT)
 # Input from pushbutton, using internal pulldown resistor. Other button wire will connect to 5V pin. 
 GPIO.setup(12, GPIO.IN, pull_up_down = GPIO.PUD_UP) 
 
-if GPIO.Input(12) == 0:
-    print("Button Pusher!  Button Pusher!")
+while True:
+    if GPIO.Input(12) == 0:
+        print("Button Pusher!  Button Pusher!")
