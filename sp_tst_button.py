3e09b89267837a3e8bb3720bb96d0fb9175f7297
pi3 <jbarry1506@gmail.com>
2020-10-30 18:55:32 -0400

    commit -m 'latest<built-in method now of type object at 0x420ab0>

--- sp_tst_button.py
+++ sp_tst_button.py
@@ -1,17 +1,21 @@
 import RPi.GPIO as GPIO
+from time import sleep
 
 # Sets pin numbering scheme to BCM
 GPIO.setmode(GPIO.BCM)
 # Logic output pin for the power strip (positive). Other power strip wire will go to GND.
 GPIO.setup(16, GPIO.OUT)
 # Input from pushbutton, using internal pulldown resistor. Other button wire will connect to 5V pin. 
-GPIO.setup(26, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(26, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 
 print(GPIO.input(26))
 while True:
-    if GPIO.input(26) == 0:
+    if GPIO.input(26) == 1:
         print("Button Pusher!  Button Pusher!")
-    elif GPIO.input(26) == 1:
+        continue
+    elif GPIO.input(26) == 0:
+        GPIO.output(16,1)
+        print("down")
+        sleep(2)
         GPIO.output(16,0)
-        print("Button released")
-        pressed = 0
+        continue
